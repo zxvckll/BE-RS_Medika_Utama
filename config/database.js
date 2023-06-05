e1ac63348d5fe372c13956a3d7785507c57fd80f
M.Syamsandi <zxvcklh@gmail.com>
2023-06-05 11:05:53 +0700

    update db config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
 import { Sequelize } from "sequelize";
 import process from "process"
+import dotenv from 'dotenv';
+import path from 'path';
 
-const dbHost = process.env.DB_HOST;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbName = process.env.DB_NAME;
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // ...
+};
+
 
 // Create connection
-const db = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
+const db = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
   dialect: "mysql"
 });
 
